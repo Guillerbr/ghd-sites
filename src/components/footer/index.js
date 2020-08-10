1ab0f1c6b258b566078f5ea77935a9d5a5702141
Guillerbr <guillerbrasilrj@gmail.com>
2020-08-09 22:36:04 -0300

    ghdsites

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -15,7 +15,7 @@ export function Rodape() {
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley o galley o
-          galley o galley o galley{" "}
+          galley o galley o galley
         </p>
       </Footer>
     </main>
