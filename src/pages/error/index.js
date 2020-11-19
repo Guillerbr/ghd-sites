9a111e0bb007a4c982f84218e5cfabb2ed706cb8
Guillerbr <guillerbrasilrj@gmail.com>
2020-11-19 17:11:45 -0300

    view-update

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -16,10 +16,11 @@ import Button from "@material-ui/core/Button";
 export function ErrorPage() {
   return (
     <main className="App">
-      {/* <img
-        src="https://image.freepik.com/fotos-gratis/imagem-cenico-da-arvore-de-abetos-vermelhos-dia-gelado-cena-invernal-calma_146671-15226.jpg"
-        alt="Logo"
-      /> */}
+
+      <img
+        src=""
+        alt="OOPS"
+      />
 
       <h1>Content not found!</h1>
       <h2>OOPS</h2>
