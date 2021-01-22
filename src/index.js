47575aa01e012f8b788256631d3f838ad3718a36
Guillerbr <guillerbrasilrj@gmail.com>
2021-01-22 16:04:43 -0300

    GHD V2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-//import App from './App';
+import "assets/scss/material-kit-react.scss?v=1.9.0";
 
-//routes
-import Routes from "./router/router.js";
+// pages for this product
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+import Map from "views/MapPage";
+
+var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Routes />
-  </React.StrictMode>,
+  <Router history={hist}>
+    <Switch>
+      {/* <Route path="/landing-page" component={LandingPage} /> */}
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/front-page" component={Components} />
+      <Route path="/map" component={Map} />
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
---
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -0,0 +1,71 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function WorkSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem cs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Entre em Contato</h2>
+          <h4 className={classes.description}>
+            Divide details about your product or agency work into parts. Write a
+            few lines about each one and contact us about any further
+            collaboration. We will responde get back to you in a couple of
+            hours.
+          </h4>
+          <form>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Seu Nome"
+                  id="name"
+                  formControlProps={{
+                    fullWidth: true,
+                  }}
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+                <CustomInput
+                  labelText="Seu Email"
+                  id="email"
+                  formControlProps={{
+                    fullWidth: true,
+                  }}
+                />
+              </GridItem>
+              <CustomInput
+                labelText="Mensagem"
+                id="message"
+                formControlProps={{
+                  fullWidth: true,
+                  className: classes.textArea,
+                }}
+                inputProps={{
+                  multiline: true,
+                  rows: 5,
+                }}
+              />
+              <GridItem xs={12} sm={12} md={4}>
+                <Button color="primary">Enviar Mensagem</Button>
+              </GridItem>
+            </GridContainer>
+          </form>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
