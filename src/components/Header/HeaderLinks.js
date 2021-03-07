b8dbedaaace0490a24a0f1928b0c3c8c7ebfff18
Guillerbr <guillerbrasilrj@gmail.com>
2021-03-06 21:08:39 -0300

    header

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -74,7 +74,7 @@ export default function HeaderLinks(props) {
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
-            href="https://twitter.com/CreativeTim?ref=creativetim"
+            href="https://twitter.com/"
             target="_blank"
             color="transparent"
             className={classes.navLink}
@@ -109,7 +109,7 @@ export default function HeaderLinks(props) {
         >
           <Button
             color="transparent"
-            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            href="https://www.instagram.com/"
             target="_blank"
             className={classes.navLink}
           >
